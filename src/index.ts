26762c916bb0ba4acdbb8beb8c7f39bb3db8e7bc
Caique Tascano <caique.tascano@jsl.com.br>
2019-04-17 18:02:26 -0300

    Adicionado ORM mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import './common/env';
 import Server from './common/server';
 import routes from './routes';
-
+import {connect, connection} from 'mongoose';
 const port = parseInt(process.env.PORT);
+
+/* connect("mongodb://localhost/teste",{useNewUrlParser: true});
+var db = connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("CONEXÂO COM BANCO ABERTA");
+}); */
+
 export default new Server()
   .router(routes)
   .listen(port);
